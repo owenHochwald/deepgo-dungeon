061141e225fe89d66892edcb2276fa5478837f9c
owenhochwald <owenhochwald@gmail.com>
2025-12-27 16:44:11 -0500

    init panel

--- main.go
+++ main.go
@@ -1,1 +1,36 @@
-package deepgo_dungeon
+package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+const (
+	screenWidth  = 320 * 2
+	screenHeight = 240 * 2
+)
+
+type Game struct {
+	count int
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
+
+func main() {
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Hello, World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
