f02228a78df454f1a07d01f1de621704ebe59f3a
owenhochwald <owenhochwald@gmail.com>
2025-12-27 19:54:55 -0500

    add random binary split logic with tests and dependencies

--- internal/utils/bsp.go
+++ internal/utils/bsp.go
@@ -1,5 +1,7 @@
 package utils
 
+import "math/rand"
+
 type Rect struct {
 	X, Y, W, H int
 }
@@ -25,4 +27,37 @@ func NewNode(x, y, w, h, level int) *Node {
 	}
 }
 
-func (n *Node) Split(minSize, maxLevel int) {}
+// Split splits the node into two children using random directions and sizes
+func (n *Node) Split(minSize, maxLevel int) bool {
+	// Base case
+	if n.Level >= maxLevel || n.Container.W < minSize*2 || n.Container.H < minSize*2 {
+		return false
+	}
+
+	// find split direction or force split
+	splitH := rand.Float64() > 0.5
+	if float64(n.Container.W) >= 1.25*float64(n.Container.H) {
+		splitH = false
+	} else if float64(n.Container.H) >= 1.25*float64(n.Container.W) {
+		splitH = true
+	}
+
+	percent := .3 + rand.Float32()*.4
+
+	// split between 30-70% of the container
+	if splitH {
+		splitPos := int(float32(n.Container.H) * percent)
+		n.Left = NewNode(n.Container.X, n.Container.Y, n.Container.W, splitPos, n.Level+1)
+		n.Right = NewNode(n.Container.X, n.Container.Y+splitPos, n.Container.W, n.Container.H-splitPos, n.Level+1)
+	} else {
+		splitPos := int(float32(n.Container.W) * percent)
+		n.Left = NewNode(n.Container.X, n.Container.Y, splitPos, n.Container.H, n.Level+1)
+		n.Right = NewNode(n.Container.X+splitPos, n.Container.Y, n.Container.W-splitPos, n.Container.H, n.Level+1)
+	}
+
+	// recursively split children
+	n.Left.Split(minSize, maxLevel)
+	n.Right.Split(minSize, maxLevel)
+
+	return true
+}
--- internal/utils/bsp_test.go
+++ internal/utils/bsp_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func assertNodeValid(t *testing.T, n *Node) {
+func assertNodeValid(t *testing.T, n *Node, maxLevel int) {
 	if n == nil {
 		return
 	}
@@ -16,45 +16,46 @@ func assertNodeValid(t *testing.T, n *Node) {
 	assert.LessOrEqual(t, n.Level, 3)
 
 	if n.Left != nil && n.Right != nil {
-		assert.GreaterOrEqual(t, n.Container.W, n.Left.Container.W+n.Right.Container.W)
-		assert.GreaterOrEqual(t, n.Container.H, n.Left.Container.H+n.Right.Container.H)
+		assert.Equal(t, n.Level+1, n.Left.Level)
+		assert.Equal(t, n.Level+1, n.Right.Level)
+
+		widthsMatch := n.Left.Container.W+n.Right.Container.W == n.Container.W
+		heightsMatch := n.Left.Container.H+n.Right.Container.H == n.Container.H
+		// sneaky xor trick from CPSC 121
+		assert.True(t, (widthsMatch || heightsMatch) && !(widthsMatch && heightsMatch))
+
+		if widthsMatch {
+			assert.Equal(t, n.Container.H, n.Left.Container.H)
+			assert.Equal(t, n.Container.H, n.Right.Container.H)
+			assert.Equal(t, n.Left.Container.X+n.Left.Container.W, n.Right.Container.X)
+		} else {
+			assert.Equal(t, n.Container.W, n.Left.Container.W)
+			assert.Equal(t, n.Container.W, n.Right.Container.W)
+			assert.Equal(t, n.Left.Container.Y+n.Left.Container.H, n.Right.Container.Y)
+		}
+
 	}
 
-	assertNodeValid(t, n.Left)
-	assertNodeValid(t, n.Right)
+	assertNodeValid(t, n.Left, maxLevel)
+	assertNodeValid(t, n.Right, maxLevel)
 }
 
 func TestNode_Split(t *testing.T) {
-	type fields struct {
-		Level     int
-		Container Rect
-		Left      *Node
-		Right     *Node
-		Room      *Rect
-	}
-	type args struct {
+	tests := []struct {
+		name     string
+		w, h     int
 		minSize  int
 		maxLevel int
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
 	}{
-		{"Basic 3 level size 10 test", fields{Level: 0, Container: Rect{X: 0, Y: 0, W: 100, H: 100}}, args{minSize: 10, maxLevel: 3}},
-	}
+		{"Basic 3 level", 100, 100, 10, 3},
+		{"Tall room", 50, 150, 10, 3},
+		{"Wide room", 150, 50, 10, 3},
+		{"Small room", 25, 25, 10, 2}}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			n := &Node{
-				Level:     tt.fields.Level,
-				Container: tt.fields.Container,
-				Left:      tt.fields.Left,
-				Right:     tt.fields.Right,
-				Room:      tt.fields.Room,
-			}
-			n.Split(tt.args.minSize, tt.args.maxLevel)
-			assertNodeValid(t, n)
-
+			n := NewNode(0, 0, tt.w, tt.h, 0)
+			n.Split(tt.minSize, tt.maxLevel) // Just one call!
+			assertNodeValid(t, n, tt.maxLevel)
 		})
 	}
 }
