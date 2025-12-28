525dfec51588400f53e2529038c8e765f11e71eb
owenhochwald <owenhochwald@gmail.com>
2025-12-27 21:01:11 -0500

    update screen scaling and integrate hallway rendering logic

--- cmd/main.go
+++ cmd/main.go
@@ -9,36 +9,29 @@ import (
 )
 
 const (
-	screenWidth  = 320 * 2
-	screenHeight = 240 * 2
+	screenWidth  = 320 * 4
+	screenHeight = 240 * 4
 )
 
 type Game struct {
-	dungeonMap [][]int // 0 -> Wall, 1 -> Floor
-	TreeRoot   *utils.Node
-	Rooms      []*utils.Node
+	TreeRoot *utils.Node
+	Rooms    []*utils.Node
+	Hallways []utils.Rect
 }
 
 func NewGame() *Game {
 	n := utils.NewNode(0, 0, screenWidth, screenHeight, 0)
 	n.Split(10, 3)
+
 	rooms := n.GetLeaves()
 
+	var hallways []utils.Rect
+	n.CreateHallways(&hallways)
+
 	return &Game{
-		dungeonMap: [][]int{
-			{0, 0, 0, 0, 1, 0, 0, 1, 0, 0},
-			{0, 1, 0, 0, 1, 0, 1, 1, 0, 0},
-			{0, 1, 1, 0, 0, 0, 0, 1, 0, 1},
-			{0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
-			{1, 0, 1, 0, 0, 0, 1, 1, 0, 0},
-			{0, 0, 1, 1, 0, 0, 0, 0, 1, 0},
-			{0, 1, 0, 0, 0, 1, 1, 0, 0, 0},
-			{0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
-			{0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
-			{1, 0, 0, 1, 0, 0, 1, 0, 0, 0},
-		},
 		TreeRoot: n,
 		Rooms:    rooms,
+		Hallways: hallways,
 	}
 }
 
@@ -46,7 +39,16 @@ func (g *Game) Update() error {
 	return nil
 }
 func (g *Game) Draw(screen *ebiten.Image) {
+	// draw hallways
+	for _, hallway := range g.Hallways {
+		vector.FillRect(
+			screen,
+			float32(hallway.X), float32(hallway.Y),
+			float32(hallway.W), float32(hallway.H),
+			color.RGBA{R: 150, G: 150, B: 150, A: 255}, false)
+	}
 
+	// draw rooms
 	for _, node := range g.Rooms {
 		vector.StrokeRect(
 			screen,
